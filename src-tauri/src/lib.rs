41a1f335e7d90bc83151708df846a024e764bbfa
wh3pp <wh3pp@proton.me>
2025-07-06 06:36:13 -0500

    feat: Implement WhatsApp web wrapper

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,43 @@
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    #[cfg(desktop)]
+    {
+        setup_desktop();
+    }
+    #[cfg(mobile)]
+    {
+        setup_mobile()
+    }
+}
+
+#[cfg(desktop)]
+fn setup_desktop() {
+    tauri::Builder::default()
+        .setup(|app| {
+            tauri::WebviewWindowBuilder::new(
+                app,
+                "main",
+                tauri::WebviewUrl::External("https://web.whatsapp.com/".parse().unwrap()),
+            )
+                .title("wuts")
+                .resizable(true)
+                .decorations(true)
+                .enable_clipboard_access()
+                .prevent_overflow()
+                .min_inner_size(800., 600.)
+                .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+                .build()?;
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[cfg(mobile)]
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+fn setup_mobile() {
+    tauri::Builder::default()
+        .init_plugin()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
